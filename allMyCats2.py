545cea107297c4eac119eeb9112d1c85875e806a
Peter Cockram <dexterpound@gmail.com>
2020-05-26 15:24:32 -0400

    Working Copy
    The last iteration made 5.26.20

--- allMyCats2.py
+++ allMyCats2.py
@@ -11,29 +11,4 @@ print('The cat names are:')
     #print(name)
 
 for i in range(len(catNames)):
-
-    #print(f"{catNames[i]} {str(i)}")
-    print(catNames[i], " : ", str(i))
-    #print(catNames[i] + " " + str(i))
-    #print(str(i),":",catNames[i])
-    #print(catNames[i], " : ", str(i))
-    #print(catNames[i], i, end=' ')
-    #print(catNames[i],end=' ' + str(i))
-    #print(catNames[i], i)
-
-
-#catNames = ['a', 'b', 'c', 'd']
-#             0    1    2    3
-#for i in range(len(catNames)):  i will go from 0, 1, 2, 3
-    #print(i) i is the index
-    #print(catNames[i]) catNames[i] is the element at index i
-
-# the output will be:
-#0
-#'a'
-#1
-#'b'
-#2
-#'c'
-#3
-#'d'
+    print(catNames[i] + " " + str(i))
